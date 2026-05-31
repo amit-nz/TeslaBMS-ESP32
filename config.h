81177f74b58e968e02f72eec932334fd3ed602bd
Amit <>
2026-06-01 11:56:58 +1200

    Initial commit

--- config.h
+++ config.h
@@ -2,8 +2,10 @@
 
 #include <Arduino.h>
 
+//extern HardwareSerial Serial1; // Leftover from back in the day when this ran on arduino
+
 //Set to the proper port for your USB connection - SerialUSB on Due (Native) or Serial for Due (Programming) or Teensy
-#define SERIALCONSOLE   SerialUSB
+#define SERIALCONSOLE   Serial
 
 //Define this to be the serial port the Tesla BMS modules are connected to.
 //On the Due you need to use a USART port (Serial1, Serial2, Serial3) and update the call to serialSpecialInit if not Serial1
@@ -30,23 +32,10 @@
 #define REG_ADC_CONV        0x34
 #define REG_ADDR_CTRL       0x3B
 
-#define MAX_MODULE_ADDR     0x3E
+#define MAX_MODULE_ADDR     0x3E //0x3E
 
 #define EEPROM_VERSION      0x10    //update any time EEPROM struct below is changed.
 #define EEPROM_PAGE         0
-
-#define DIN1                55
-#define DIN2                54
-#define DIN3                57
-#define DIN4                56
-#define DOUT4_H             2
-#define DOUT4_L             3
-#define DOUT3_H             4
-#define DOUT3_L             5
-#define DOUT2_H             6
-#define DOUT2_L             7
-#define DOUT1_H             8
-#define DOUT1_L             9
 
 typedef struct {
     uint8_t version;
@@ -61,3 +50,41 @@ typedef struct {
     float balanceVoltage;
     float balanceHyst;
 } EEPROMSettings;
+
+/*
+These pin definitions are specific to the board that's in use.
+It appears they're not in use, however if the pin definitions are commented out,
+it makes the code in SystemIO.cpp not work, which breaks the whole thing - 
+i.e. logger throws "No modules responded to findBoards() - need to find out why later."
+*/
+// -- These are the pin definitions for ESP32-S3-WROOM1, N16R8 Variant--
+#define DIN1   5    // input
+#define DIN2   6    // input
+#define DIN3   7    // input
+#define DIN4   8    // input
+
+#define DOUT1_H 15
+#define DOUT1_L 9
+#define DOUT2_H 10
+#define DOUT2_L 11
+#define DOUT3_H 12
+#define DOUT3_L 13
+#define DOUT4_H 14
+#define DOUT4_L 18
+
+/* 
+// -- These are the pins for ESP32-WROOM-32D DevKit board HW-394 --
+#define DIN1   34   // input only
+#define DIN2   35   // input only
+#define DIN3   32   // input only
+#define DIN4   33   // input only
+
+#define DOUT1_H 25
+#define DOUT1_L 26
+#define DOUT2_H 27
+#define DOUT2_L 14
+#define DOUT3_H 12
+#define DOUT3_L 13
+#define DOUT4_H 15
+#define DOUT4_L 9 // changed this to 9 from 4, suspect its making LED not work properly
+*/
