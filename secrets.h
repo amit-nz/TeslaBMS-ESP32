fa232d13bd55b76c1d6a1572b48d2746672b9569
Amit <>
2026-06-01 11:35:37 +1200

    Add secrets.h

---
+++ secrets.h
@@ -0,0 +1,15 @@
+#define SECRET_MQTT_SERVER_IP "192.168.1.2"
+#define SECRET_MQTT_USER "mqtt_user"
+#define SECRET_MQTT_PASSWORD "mqtt_password"
+
+#define SECRET_WIFI_SSID "WIFI_Network_Name"
+#define SECRET_WIFI_PASSWORD "WIFI_PSK"
+
+#define SECRET_FTP_SERVER_IP "192.168.1.3"
+#define SECRET_FTP_USER "ftpuser"
+#define SECRET_FTP_PASSWORD "ftpuser_password"
+
+#define SECRET_WEBUI_USER "admin"
+#define SECRET_WEBUI_PASS "changeme123"
+
+#define SECRET_MQTT_TOPIC "homeassistant/sensor/bms/"
